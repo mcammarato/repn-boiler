a0230cf3cd1c2a91582e487d79875dc1995d69a7
Mike Cammarato <mcammarato@gmail.com>
2021-01-11 23:54:08 -0500

    running on ubuntu

--- src/server/routes/api/adventureworks.js
+++ src/server/routes/api/adventureworks.js
@@ -3,7 +3,7 @@ const db = require('../../../../knexfile');
 
 const router = express.Router();
 
-// /api/v1/getAddresses
+// /api/v1/aw/getAddresses
 router.get('/getAddresses', (req, res) => {
   db.select()
   .from('person.address')
@@ -11,6 +11,13 @@ router.get('/getAddresses', (req, res) => {
   .then((data) => {
     res.send(data);
   });
+});
+
+// api/v1/aw/test
+router.get('/test', (req, res) => {
+  res.json({
+    testProp: "test value"
+  })
 });
 
 module.exports = router;

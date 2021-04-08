3e94ae3beed1d30e810bdf12ba44fd6797de5afb
cammo <cammo@cammos-MBP.fios-router.home>
2021-04-08 00:34:21 -0400

    cleaned up file

--- knexfile.js
+++ knexfile.js
@@ -1,15 +1,5 @@
 require('dotenv').config();
 
-// const pg = require('knex')({
-//   client: 'pg',
-//   connection: {
-//     host: process.env.PGHOST,
-//     user: process.env.PGUSER,
-//     password: process.env.PGPASSWORD,
-//     database: process.env.PGDATABASE,
-//   },
-// });
-
 const db = require('knex')({
   client: 'pg',
   version: '7.2.0',
